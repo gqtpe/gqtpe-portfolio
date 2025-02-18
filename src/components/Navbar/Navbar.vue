25bebcb3faf5f06b0ce75e9b13a40778ac747e16
gqtpe <work.temirtas@gmail.com>
2025-02-18 23:50:49 +0500

    portfolio v0.0.1(building base):Navbar base added

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav class="flex justify-around w-1/2 ">
+    <div class="flex link justify-center grow h100">
+      <a  href="#">Home</a>
+    </div>
+    <div class="flex link justify-center grow h100">
+      <a  href="#">About</a>
+    </div>
+    <div class="flex link justify-center grow h100">
+      <a  href="#">Projects</a>
+    </div>
+    <div class="flex link justify-center grow h100">
+      <a  href="#">Contacts</a>
+    </div>
+
+  </nav>
+</template>
+
+<style scoped>
+
+.link a{
+  padding: 0.4rem 0.25rem;
+  transition: background-color, 0.2s ease-in;
+
+}
+
+.link:hover a{
+  color: var(--black);
+  background-color: var(--white);
+  border-radius: 0.125rem;
+}
+</style>
