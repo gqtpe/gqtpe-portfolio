7c365df7769f2ba1015ca24229c68ac11d12e322
gqtpe <work.temirtas@gmail.com>
2025-02-20 11:11:44 +0500

    portfolio v0.0.1(router):router created

---
+++ src/router.ts
@@ -0,0 +1,13 @@
+import Home from "./components/pages/Home/Home.vue";
+import About from "./components/pages/About/About.vue";
+import {createRouter, createWebHistory} from "vue-router";
+
+
+const routes = [
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+export default router;
