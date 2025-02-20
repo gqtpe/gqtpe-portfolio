f28aff47c313b95bcb14edb26fbc31f926eb9597
gqtpe <work.temirtas@gmail.com>
2025-02-20 11:14:51 +0500

    portfolio v0.0.1:Button UI component added

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+type Props = {
+  color: 'primary' | 'secondary'
+}
+const props = defineProps<Props>()
+const secondaryClasses = "bg-secondary-500 hover:bg-primary-500"
+const primaryClasses = "bg-primary-500 hover:bg-secondary-500"
+</script>
+
+<template>
+  <button :class="`${props.color==='primary'?primaryClasses:secondaryClasses} transition-colors text-white font-bold py-2 px-4 rounded-full`">
+    <slot/>
+  </button>
+</template>
