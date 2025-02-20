ef38374a21a9f89f0dfe5cb09ee542d5288d45cb
gqtpe <work.temirtas@gmail.com>
2025-02-20 11:15:17 +0500

    portfolio v0.0.1:Footer created

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,16 @@
+<script setup>
+import linkedin from "../../assets/svg/linkedin-1-svgrepo-com.svg"
+import github from "../../assets/svg/github-svgrepo-com.svg"
+import telegram from "../../assets/svg/telegram-svgrepo-com.svg"
+import {links} from "../../router.js";
+
+</script>
+<template>
+<footer class="bg-black h-[60px] text-white">
+
+</footer>
+</template>
+
+<style scoped>
+
+</style>
