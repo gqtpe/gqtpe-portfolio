1bce0aeab6b4b92610d7dcbc3c5bef2b716c07a7
gqtpe <work.temirtas@gmail.com>
2025-02-20 11:15:45 +0500

    portfolio v0.0.1:Footer styles added

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -12,5 +12,26 @@ import {links} from "../../router.js";
 </template>
 
 <style scoped>
-
+.footer{
+  border: 1px dashed white;
+}
+.link{
+  display: flex;
+  align-items: center;
+  transition: color 0.5s ease-in;
+  gap: var(--spacing);
+  img{
+    height: 1.5rem;
+    width: 1.5rem;
+    box-shadow: var(--shadow);
+    border-radius: 50%;
+  }
+}
+.link:hover{
+  color: var(--color-gray-300)
+}
+.link:active{
+  transition: color 0.1s ease-in-out;
+  color: var(--secondary-color-500);
+}
 </style>
