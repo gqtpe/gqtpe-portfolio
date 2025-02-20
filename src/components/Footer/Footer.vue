cb0603d43ceaae6a4e3857e47e520e0994bbc1e8
gqtpe <work.temirtas@gmail.com>
2025-02-20 11:15:55 +0500

    portfolio v0.0.1:footer filled with content

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -6,9 +6,16 @@ import {links} from "../../router.js";
 
 </script>
 <template>
-<footer class="bg-black h-[60px] text-white">
-
-</footer>
+  <footer class="bg-black h-[60px] text-white">
+    <div class="page-wrapper footer h-full flex justify-around items-center">
+      <span>© 2025 All rights reserved.</span>
+      <div class="flex gap-3 text-gray-500 ">
+        <a :href="links.linkedin" target="_blank" class="link"><img :src="linkedin">Linkedin</a>
+        <a :href="links.github" target="_blank" class="link"><img :src="github">Github</a>
+        <a :href="links.telegram" target="_blank" class="link"><img :src="telegram">Telegram</a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
