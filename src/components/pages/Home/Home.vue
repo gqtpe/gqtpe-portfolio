f402e19a196aba48741752287dd1b7b898c7107e
gqtpe <work.temirtas@gmail.com>
2025-02-21 11:43:44 +0500

    portfolio v0.0.1:responsive home component+typing integrated

--- src/components/pages/Home/Home.vue
+++ src/components/pages/Home/Home.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
 import Button from "../../Button.vue";
+import Typing from "../../Typing.vue";
+
+const items = [
+    'web developer',
+    'frontend developer',
+    'software engineer',
+]
 </script>
 
 <template>
-  <div class="flex w-full h-full items-center  text-white">
-    <div class="bg-black p-8 inline-flex flex-col gap-[10px] p-12 items-start">
+  <section class="flex w-full h-full items-center text-white max-md:justify-center">
+    <div class="bg-black p-8 inline-flex flex-col gap-[10px] p-12 items-start max-md:w-full">
       <h6 class="ellipse1 uppercase text-lg font-bold">hello, there</h6>
-      <h1 class="ellipse2 uppercase text-7xl font-black">i'm Nursain Temirtas</h1>
-      <h6 class="uppercase text-lg font-bold">web-developer</h6>
+      <h1 class="ellipse2 uppercase font-black lg:text-8xl sm:text-6xl max-md:text-7xl max-sm:text-3xl">i'm Nursain Temirtas</h1>
+      <h6 class="uppercase font-bold">
+        <Typing
+            :items=items
+        />
+      </h6>
       <Button color="secondary">Download CV</Button>
     </div>
-  </div>
+  </section>
 </template>
 <style scoped>
+
 .ellipse1,.ellipse2 {
   position: relative;
   z-index: 1;
@@ -20,8 +32,12 @@ import Button from "../../Button.vue";
 .ellipse1:before, .ellipse2:after {
   content: "";
   position: absolute;
-  width: 3rem;
-  height: 3rem;
+  width: 3.5vw;
+  height: 3.5vw;
+  @media(max-width: 480px){
+    width:8vw;
+    height:8vw;
+  }
 }
 .ellipse1:before {
   background-color: var(--color-secondary-500);
