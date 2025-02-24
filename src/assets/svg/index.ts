561fbee95cafc8b9660e37cf497dee4fee4b1afd
gqtpe <work.temirtas@gmail.com>
2025-02-24 21:50:25 +0500

    portfolio v0.0.1:centralized imports for svg

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,65 @@
+import github from "./footer/github-svgrepo-com.svg";
+import linkedin from "./footer/linkedin-1-svgrepo-com.svg";
+import telegram from "./footer/telegram-svgrepo-com.svg";
+
+import antdesign from "./about/ant_design.svg";
+import css from "./about/css_old.svg";
+import figma from "./about/figma.svg";
+import html from "./about/html5.svg";
+import javascript from "./about/javascript.svg";
+import jwt from "./about/jwt.svg";
+import materialui from "./about/materialui.svg";
+import nodejs from "./about/nodejs.svg";
+import postman from "./about/postman.svg";
+import prettier from "./about/prettier.svg";
+import react from "./about/react.svg";
+import reactrouter from "./about/reactrouter.svg";
+import redux from "./about/redux.svg";
+import sass from "./about/sass.svg";
+import socketio from "./about/socketio-light.svg";
+import storybook from "./about/storybook.svg";
+import styledcomponents from "./about/styledcomponents.svg";
+import tailwindcss from "./about/tailwindcss.svg";
+import tanstack from "./about/tanstack.svg";
+import typescript from "./about/typescript.svg";
+import vercel from "./about/vercel.svg";
+import vite from "./about/vitejs.svg";
+import vue from "./about/vue.svg";
+
+const aboutSVGs = {
+    html,
+    css,
+    sass,
+    javascript,
+    typescript,
+    react,
+    vue,
+    vite,
+    redux,
+    reactrouter,
+    vercel,
+    storybook,
+    socketio,
+    antdesign,
+    figma,
+    materialui,
+    jwt,
+    nodejs,
+    postman,
+    prettier,
+    styledcomponents,
+    tailwindcss,
+    tanstack,
+}
+
+const footerSVGs = {
+    github,
+    linkedin,
+    telegram
+}
+
+export {
+    footerSVGs,
+    aboutSVGs
+}
+
