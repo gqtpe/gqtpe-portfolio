aa4ed175977909772cbb31ad5bd0ec283ed9da94
gqtpe <work.temirtas@gmail.com>
2025-02-24 21:51:18 +0500

    portfolio v0.0.1:projects added to router

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import Home from "./components/pages/Home/Home.vue";
 import About from "./components/pages/About/About.vue";
 import {createRouter, createWebHistory} from "vue-router";
+import Projects from "./components/pages/Projects/Projects.vue";
 
 
 const routes = [
     {path: '/', component: Home},
     {path: '/about', component: About},
+    {path: '/projects', component: {Projects}}
 ]
 export const links ={
     linkedin: "https://www.linkedin.com/in/temirtas-nursayyn-4090a0350/",
