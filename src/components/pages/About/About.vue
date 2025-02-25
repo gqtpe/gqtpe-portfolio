7967b15980be4a13147e8956593be2849b8da4a3
gqtpe <work.temirtas@gmail.com>
2025-02-25 16:26:03 +0500

    portfolio v0.0.2:About page created

--- src/components/pages/About/About.vue
+++ src/components/pages/About/About.vue
@@ -1,43 +1,48 @@
-<script setup lang="ts">
-import {aboutSVGs} from "../../../assets/svg";
+<script setup>
+import Side from "../../Side.vue";
+import {aboutSVGs} from "../../../assets/svg/index.js";
+import Button from "../../Button.vue";
 </script>
 
 <template>
-  <section class="flex items-center px-4">
-    <div class="block bg-black p-4">
-      <h2 class="text-5xl uppercase font-bold">About</h2>
-      <p>
-        Iam a Front-end developer with one year of freelance experience(1 year). I have strong skills in modern
-        technologies like React, Vite,
-        TypeScript, and Redux Toolkit. I build scalable and well-structured applications. I like learning new things,
-        discussing ideas with the team,
-        and finding the best solutions together. I also have some Back-end experience, which helps me understand the
-        full development process and
-        work better with Back-end developers. I'm constantly developing person, open-minded and dedicated
-      </p>
-
-
-    </div>
-    <div>
-      <h6 class="text-2xl underline text-black">Libraries and Tools</h6>
-      <div class="flex flex-wrap gap-[10px]">
-        <div v-for="item in aboutSVGs" class="skill shadow-2xl">
-          <img class="w-[3rem] h-[3rem]" :src="item" alt="icon">
+  <section class="flex text-black">
+    <Side title="About"/>
+    <div class="flex flex-col gap-4 items-start">
+      <h3 class="inline-block text-xl uppercase text-white bg-primary-500 p-1">who i am</h3>
+      <div class="p-4">
+        <h3 class="uppercase font-bold text-4xl">i'm Nursain Temirtas</h3>
+        <p class="w-100">
+          A Front-end developer with one year of freelance experience(1 year). I have strong skills in modern
+          technologies like React, Vite,
+          TypeScript, and Redux Toolkit. I build scalable and well-structured applications. I like learning new things,
+          discussing ideas with the team,
+          and finding the best solutions together. I also have some Back-end experience, which helps me understand the
+          full development process and
+          work better with Back-end developers. I'm constantly developing person, open-minded and dedicated
+        </p>
+        <div>
+          <div class="flex flex-wrap w-100 gap-[10px]">
+            <div v-for="item in aboutSVGs" class="skill">
+              <img class="w-[2rem] h-[2rem]" :src="item" alt="icon">
+            </div>
+          </div>
         </div>
+        <Button color="secondary">Download CV</Button>
       </div>
     </div>
+
   </section>
 </template>
 
 <style scoped>
-.skill{
+.skill {
   background: white;
   padding: 0.75rem;
+  border: 2px solid black;
   border-radius: 50%;
 
 }
+
 .block {
-  width: 50%;
-  height: 80%;
 }
 </style>
