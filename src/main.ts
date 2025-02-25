6067b888161fb5c9cc0ba9b059951dce76bfa92c
gqtpe <work.temirtas@gmail.com>
2025-02-25 16:20:20 +0500

    portfolio v0.0.2: oh-vue icons added

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import './styles/main.css'
 import App from './App.vue'
 import router from "./router.ts";
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { FaExpand, FaExternalLinkSquareAlt   } from "oh-vue-icons/icons";
 
 createApp(App)
     .use(router)
+    .component("v-icon", OhVueIcon)
     .mount('#app')
