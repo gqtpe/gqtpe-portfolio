39f57f9b2c5eb1af13fb5a613c7dd3a44da8d56c
gqtpe <work.temirtas@gmail.com>
2025-02-26 23:10:24 +0500

    portfolio v0.0.2: links moved to separate file

---
+++ src/app/links.ts
@@ -0,0 +1,7 @@
+export const links ={
+    linkedin: "https://www.linkedin.com/in/temirtas-nursayyn-4090a0350/",
+    github: "https://github.com/gqtpe",
+    telegram: "https://t.me/gqtpe",
+    trello: "https://gqtpe.github.io/trello",
+    spotify: "https://spotify-gqtpe.vercel.app/home"
+}
