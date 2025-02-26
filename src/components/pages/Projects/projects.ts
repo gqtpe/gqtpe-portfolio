ccce36e576b9ad596aecfc6c62a8ae0589ede652
gqtpe <work.temirtas@gmail.com>
2025-02-26 23:09:15 +0500

    portfolio v0.0.2: restructured array to associative array

---
+++ src/components/pages/Projects/projects.ts
@@ -0,0 +1,38 @@
+import {links} from "../../../app/links.ts";
+
+export type Project = {
+    title: string
+    description: string
+    id: string
+    icon: string
+    preview?: string
+    path?: string
+}
+export type Projects ={
+    [key:string]:Project
+}
+const projects: Projects = {
+    ['trello']:{
+        id: 'trello',
+        title: 'Trello',
+        description: 'Todolist is a pet project for task management, created using modern web technologies. This project allows for convenient task management and supports user authorization.',
+        icon: "co-trello",
+        preview: links.trello,
+        path: '/project/trello'
+    },
+    ['spotify']:{
+        id: 'spotify',
+        title: 'Spotify',
+        description: 'A Spotify clone built with Next.js, offering a similar user experience to the original Spotify platform. This project integrates with the Spotify Web API to provide music streaming, playlists, and user interaction features.',
+        icon: "bi-spotify",
+        preview: links.spotify,
+        path: '/project/spotify'
+    },
+    ['portfolio']:{
+        id: 'portfolio',
+        title: 'Portfolio',
+        description: ' A Portfolio application build with Vue.',
+        icon: "bi-file-earmark-person",
+    },
+}
+export default projects;
