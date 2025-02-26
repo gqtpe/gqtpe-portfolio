b7d2a3e7e785e1cba8895972301fb2bd2a2690d9
gqtpe <work.temirtas@gmail.com>
2025-02-26 23:18:12 +0500

    portfolio v0.0.2: Project component added

---
+++ src/components/pages/Projects/Project/Project.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+
+const route = useRoute()
+console.log(route)
+</script>
+
+<template>
+  <section class="text-black">
+  {{JSON.stringify(route, null, 2)}}
+  </section>
+</template>
+
+<style scoped>
+
+
+</style>
