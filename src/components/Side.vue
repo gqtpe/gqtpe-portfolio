4b841c9a545e1f927c0406eb6d3b03d77dcfbdc9
gqtpe <work.temirtas@gmail.com>
2025-02-26 23:04:41 +0500

    portfolio v0.0.2:aside component

--- src/components/Side.vue
+++ src/components/Side.vue
@@ -3,9 +3,9 @@ defineProps({title:String})
 </script>
 
 <template>
-  <div class="flex bg-black p-4 w-1/4 h-full ">
+  <aside class="flex bg-black p-4 w-1/4 h-full ">
     <div class="border-b-2 border-l-2 border-white w-full flex items-end p-4">
       <h2 class="text-xl uppercase font-bold text-white">{{ title }}</h2>
     </div>
-  </div>
+  </aside>
 </template>
