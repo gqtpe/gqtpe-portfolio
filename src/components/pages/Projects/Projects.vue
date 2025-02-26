c50f9a709a763633a8df94e72270929a3b37f8e0
gqtpe <work.temirtas@gmail.com>
2025-02-26 23:18:46 +0500

    portfolio v0.0.2: Projects component refactored

--- src/components/pages/Projects/Projects.vue
+++ src/components/pages/Projects/Projects.vue
@@ -1,30 +1,23 @@
 <script setup lang="ts">
-
-import Side from "../../Side.vue";
 import ProjectCard from "./ProjectCard.vue";
 import projects from "./projects.ts";
 </script>
 
 <template>
-  <section class="projects flex">
-    <Side title="my Projects"/>
-    <div class="flex flex-col overflow-hidden h-full w-3/4 text-black items-start">
-
-      <h3 class="inline-block text-xl uppercase text-white bg-secondary-500 p-1">latest work</h3>
-      <div class="flex flex-col items-start p-8 gap-8 h-full">
-        <h3 class="uppercase font-bold text-2xl border-b-4 border-t-4 border-primary-500 border-dashed">my projects</h3>
-        <div class="cards grid grid-cols-4 gap-2 ">
-          <ProjectCard v-for="project in projects" :title="project.title" :image="project.image"
-                       :description="project.description" :key="project.id"/>
+  <section class="text-black">
+      <h3 class="text-highlight-1">latest work</h3>
+      <h6 class="text-comment px-8">"Hi! I build scalable, user-friendly web applications. Here are some
+        of my latest projects."</h6>
+      <div class="flex flex-col items-start p-8 gap-8">
+        <h3 class="text-highlight-2">my projects</h3>
+        <div class="cards w-full grid grid-cols-3 gap-2 ">
+          <ProjectCard v-for="project in projects"
+                       :project="project"
+                       :key="project.id"
+          />
         </div>
       </div>
-    </div>
   </section>
 </template>
 
-<style scoped>
-.projects {
-
-}
-
-</style>
+<style scoped></style>
