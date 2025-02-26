61a71cf4186b291c483723fae30e7e4374226d20
gqtpe <work.temirtas@gmail.com>
2025-02-26 23:09:35 +0500

    portfolio v0.0.2: oh-vue icon adding moved to separate file

---
+++ src/app/ohh-vue.ts
@@ -0,0 +1,7 @@
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import {BiFileEarmarkPerson, BiSpotify, CoTrello, FaExpand, FaExternalLinkSquareAlt} from "oh-vue-icons/icons";
+
+addIcons(FaExpand, FaExternalLinkSquareAlt, CoTrello,BiFileEarmarkPerson , BiSpotify);
+
+
+export {OhVueIcon}
