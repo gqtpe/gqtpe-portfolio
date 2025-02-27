f92660bbd0b037eedc3d897b58aa15ef1c0a358d
gqtpe <work.temirtas@gmail.com>
2025-02-27 21:49:20 +0500

    portfolio v0.0.3: section for About created

---
+++ src/components/pages/About/Section.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+type Props = {
+  title: string
+  period: string
+  body: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="w-full">
+    <div class="subtitle flex justify-between"><span>{{ title }}</span> <span>{{period}}</span></div>
+    <p class="body">{{body}}</p>
+  </div>
+</template>
+<styles scoped>
+
+</styles>
