3c24c9ddadccaee8ac7103b10ce41cf3b269aa53
gqtpe <work.temirtas@gmail.com>
2025-03-04 21:02:04 +0500

    portfolio v0.0.4: home page styles refactored

--- src/components/pages/Home/Home.vue
+++ src/components/pages/Home/Home.vue
@@ -13,13 +13,14 @@ const items = [
 
 <template>
   <section class="flex items-center text-white max-md:justify-center">
-    <div class="bg-black p-8 inline-flex flex-col gap-[10px] p-12 items-start max-md:w-full text-white">
+    <div class="bg-black p-8 flex flex-col gap-[10px] items-start max-md:w-full text-white">
       <h6 class="ellipse1 uppercase text-lg font-bold">
 
         <DecryptedText
             text="hello, there"
             animateOn="view"
             revealDirection="start"
+
         />
       </h6>
       <h1 class="ellipse2 uppercase font-black lg:text-7xl sm:text-6xl max-md:text-7xl max-sm:text-3xl">
@@ -49,12 +50,9 @@ const items = [
 .ellipse1:before, .ellipse2:after {
   content: "";
   position: absolute;
-  width: 3.5vw;
-  height: 3.5vw;
-  @media(max-width: 480px){
-    width:8vw;
-    height:8vw;
-  }
+  width:2rem;
+  height:2rem;
+
 }
 .ellipse1:before {
   background-color: var(--color-secondary-500);
