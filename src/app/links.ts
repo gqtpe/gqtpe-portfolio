6aa938ac7d19323154150b758b9cc6877bc2ebef
gqtpe <work.temirtas@gmail.com>
2025-03-05 10:52:07 +0500

    portfolio v0.0.4(oh-vue): project images import

--- src/app/links.ts
+++ src/app/links.ts
@@ -1,3 +1,17 @@
+import type {Image, ProjectName} from "@/components/pages/Projects/types.ts";
+import trelloHome from "@/assets/img/trello/gqtpe.github.io_trello_home.png";
+import trelloLogin from "@/assets/img/trello/gqtpe.github.io_trello_login.png";
+
+import spotifyHome from "@/assets/img/spotify/spotify-gqtpe.vercel.app_home.png";
+import spotifyLogin from "@/assets/img/spotify/spotify-gqtpe.vercel.app_login.png";
+import spotifyAlbum from "@/assets/img/spotify/spotify-gqtpe.vercel.app_album.png";
+import spotifyLiked from "@/assets/img/spotify/spotify-gqtpe.vercel.app_liked.png";
+import spotifySearch from "@/assets/img/spotify/spotify-gqtpe.vercel.app_search.png";
+import spotifySearchStart from "@/assets/img/spotify/spotify-gqtpe.vercel.app_seach-start.png";
+import spotifyTrack from "@/assets/img/spotify/spotify-gqtpe.vercel.app_track-page.png";
+import spotifyArtists from "@/assets/img/spotify/spotify-gqtpe.vercel.artists-search.png";
+import spotifyAccountConfirmation from "@/assets/img/spotify/user-account-confirmation.png";
+
 export const links ={
     linkedin: "https://www.linkedin.com/in/temirtas-nursayyn-4090a0350/",
     github: "https://github.com/gqtpe",
