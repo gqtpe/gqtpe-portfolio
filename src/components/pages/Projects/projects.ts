11f76afe99a6bd01da2c44db8b5ad8181cb5ac20
gqtpe <work.temirtas@gmail.com>
2025-03-05 10:57:51 +0500

    portfolio v0.0.4: project object expanded

--- src/components/pages/Projects/projects.ts
+++ src/components/pages/Projects/projects.ts
@@ -1,29 +1,43 @@
-import {links} from "../../../app/links.ts";
+import {links, projectImages} from "@/app/links.ts";
+import type {Projects} from "@/components/pages/Projects/types.ts";
+
 
 
 
 const projects: Projects = {
-    ['trello']:{
+    ["trello"]: {
         id: 'trello',
         title: 'Trello',
         description: 'Todolist is a pet project for task management, created using modern web technologies. This project allows for convenient task management and supports user authorization.',
         icon: "co-trello",
-        preview: links.trello,
-        path: '/project/trello'
+        links: {
+            url: links.trello,
+            path: '/project/trello',
+            github: links.trelloGithub
+        },
+        images: projectImages["trello"]
     },
-    ['spotify']:{
+    ["spotify"]: {
         id: 'spotify',
         title: 'Spotify',
         description: 'A Spotify clone built with Next.js, offering a similar user experience to the original Spotify platform. This project integrates with the Spotify Web API to provide music streaming, playlists, and user interaction features.',
         icon: "bi-spotify",
-        preview: links.spotify,
-        path: '/project/spotify'
+        links: {
+            url: links.spotify,
+            path: '/project/spotify',
+            github: links.spotifyGithub
+        },
+        images: projectImages["spotify"]
     },
-    ['portfolio']:{
+    ["portfolio"]: {
         id: 'portfolio',
         title: 'Portfolio',
         description: ' A Portfolio application build with Vue.',
         icon: "bi-file-earmark-person",
+        links: {
+            github: links.spotifyGithub
+        },
+        images: []
     },
 }
 export default projects;
