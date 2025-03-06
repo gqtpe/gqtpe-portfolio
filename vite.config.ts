ab421166d755bbcf9d8c95475f853de41ca877fb
gqtpe <work.temirtas@gmail.com>
2025-03-06 20:21:10 +0500

    portfolio v0.0.4(fixed tsconfig): vite.config.ts configured correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
-import {fileURLToPath} from "node:url";
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            "@": fileURLToPath(new URL('./src', import.meta.url)),
+            "@": path.resolve(__dirname, './src'),
         },
     },
 })
