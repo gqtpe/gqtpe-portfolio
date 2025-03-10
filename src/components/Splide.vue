c96e2d49527c5a1189e2c18493671aa37eb88397
gqtpe <work.temirtas@gmail.com>
2025-03-10 13:41:18 +0500

    portfolio v0.0.5: splide component created

---
+++ src/components/Splide.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type {Image} from "@/components/pages/Projects/types.ts";
+import {Options} from "@splidejs/splide";
+
+defineProps<{ images: Image[] }>()
+const splideOption: Options = {
+  type: 'loop',
+  perPage: 1,
+  autoplay: false,
+  lazyLoad: true,
+  arrows: true,
+}
+</script>
+
+<template>
+  <splide-wrapper :options="splideOption">
+    <slide v-for="image in images" :key="image.id">
+      <img :src="image.url" alt="Image 1"/>
+    </slide>
+  </splide-wrapper>
+
+</template>
+
+<style>
+.splide__pagination__page.is-active {
+  background: var(--color-primary-500);
+  opacity: 1;
+}
+
+.splide__arrow {
+  background: var(--color-primary-500);
+
+
+  &:active {
+    background: var(--color-secondary-500);
+    opacity: 1;
+  }
+}
+
+</style>
