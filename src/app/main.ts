9150301e91fb76ad0ca9660d2c2ab4c0680b80aa
gqtpe <work.temirtas@gmail.com>
2025-03-11 21:07:34 +0500

    portfolio v0.0.5: splideJS components added

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,10 +3,12 @@ import { createApp } from 'vue';
 import './main.css';
 import App from './App.vue';
 import {OhVueIcon} from "./ohh-vue";
-
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
 
 
 createApp(App)
     .use(router)
     .component("v-icon", OhVueIcon)
+    .component("splide-wrapper",Splide)
+    .component("slide", SplideSlide)
     .mount('#app')
