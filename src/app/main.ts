24e47c223b8cab299eae44a1b22eed35833ed89e
gqtpe <work.temirtas@gmail.com>
2025-03-12 22:37:51 +0500

    portfolio v0.0.5: vueuse initialization added

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,9 +1,11 @@
 import router from "./router";
 import { createApp } from 'vue';
 import './main.css';
+import '@/styles/tailwind.css';
 import App from './App.vue';
 import {OhVueIcon} from "./ohh-vue";
 import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import {MotionPlugin} from "@vueuse/motion";
 
 
 createApp(App)
@@ -11,4 +13,5 @@ createApp(App)
     .component("v-icon", OhVueIcon)
     .component("splide-wrapper",Splide)
     .component("slide", SplideSlide)
+    .use(MotionPlugin)
     .mount('#app')
