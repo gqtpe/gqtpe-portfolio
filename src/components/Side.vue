b78274d101fc86064d47881e5023f357299ba4be
gqtpe <work.temirtas@gmail.com>
2025-03-13 13:55:56 +0500

    portfolio v0.0.5:vueuse/motion preset animation added

--- src/components/Side.vue
+++ src/components/Side.vue
@@ -3,7 +3,7 @@ defineProps({title:String})
 </script>
 
 <template>
-  <aside class="flex bg-black p-4 w-1/4 h-full ">
+  <aside  v-motion-fade-visible class="flex bg-black p-4 w-1/4 h-full">
     <div class="border-b-2 border-l-2 border-white w-full flex items-end p-4">
       <h2 class="text-xl uppercase font-bold text-white">{{ title }}</h2>
     </div>
