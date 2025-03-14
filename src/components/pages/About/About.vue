91c3b8768336a4cc0f3676484ed6383dbfa83f64
gqtpe <work.temirtas@gmail.com>
2025-03-14 23:47:18 +0500

    portfolio v0.0.6: getSlidePreset used

--- src/components/pages/About/About.vue
+++ src/components/pages/About/About.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import ava from "@/assets/img/ava_corrected.png"
 import cv from "@/assets/CV.pdf";
+import ava from "@/assets/img/ava_corrected.png"
 import Button from "@/components/Button.vue";
 import Section from "@/components/pages/About/Section.vue";
 import DecryptedText from "@/components/bits/DecryptedText.vue";
 import FallingItems from "@/components/bits/FallingItems.vue";
+import {getSlidePreset} from "@/motion/motion-presets.ts";
 </script>
 
 <template>
@@ -12,8 +13,14 @@ import FallingItems from "@/components/bits/FallingItems.vue";
     <h3 class="text-highlight-1 mb-4 max-sm:mb-0 self-start">who i am</h3>
     <div class="flex p-4 gap-4 mb-8  md:items-start max-md:flex-col items-center">
       <div class="flex flex-col items-center about__image basis-1/6 max-md:w-[50%]">
-        <img v-motion-roll-visible-left loading="lazy" class="" :src="ava" alt="">
-        <a class="mt-4" :href="cv" download="CV" target='_blank'>
+        <img v-motion-roll-visible-once-top loading="lazy" class="" :src="ava" alt="">
+        <a
+            v-motion="getSlidePreset({delay: 1000, initDistance: 50})"
+            class="mt-4"
+            :href="cv"
+            download="CV"
+            target='_blank'
+        >
           <Button color="secondary">Download CV</Button>
         </a>
       </div>
@@ -21,7 +28,7 @@ import FallingItems from "@/components/bits/FallingItems.vue";
         <h3 class="uppercase mb-6 font-bold text-2xl">
           <DecryptedText text="i'm Nursain Temirtas" animateOn="view" use-original-chars-only></DecryptedText>
         </h3>
-        <p v-motion-slide-visible-bottom class="text-sm text-gray-600">
+        <p v-motion="getSlidePreset({delay: 200, variant: 'visibleOnce'})" class="text-sm text-gray-600">
           A Front-end developer with one year of freelance experience(1 year). I have strong skills in modern
           technologies like React, Vite,
           TypeScript, and Redux Toolkit. I build scalable and well-structured applications. I like learning new things,
@@ -31,17 +38,17 @@ import FallingItems from "@/components/bits/FallingItems.vue";
           work better with Back-end developers. I'm constantly developing person, open-minded and dedicated
         </p>
         <div class="divider"/>
-        <div  class="grid grid-cols-2 gap-8 max-md:flex flex-col">
-          <div v-motion-slide-visible-bottom>
-            <h3 class="section-title">Experience</h3>
+        <div class="grid grid-cols-2 gap-8 max-md:flex flex-col">
+          <div v-motion="getSlidePreset({delay: 400})">
+            <DecryptedText text="experience" use-original-chars-only animateOn="hover" class="section-title"/>
             <Section
                 title="Upwork"
                 period="2023 - current"
                 body="Developed web applications using React and Vite. Independently designed architecture, integrated APIs, and optimized performance. Worked with TypeScript, Redux Toolkit, and testing (TDD/BDD)"
             />
           </div>
-          <div v-motion-slide-visible-bottom>
-            <h3 class="section-title">Education</h3>
+          <div v-motion="getSlidePreset({delay: 600})">
+            <DecryptedText text="education" use-original-chars-only animateOn="hover" class="section-title"/>
             <Section
                 title="Kozybaev-Arizona University"
                 period="2022 - current"
@@ -51,8 +58,9 @@ import FallingItems from "@/components/bits/FallingItems.vue";
         </div>
       </div>
     </div>
-    <div  class="stack flex flex-col items-center grow-1">
-      <h4 v-motion-slide-visible-bottom class="section-title text-highlight-2 inline">My stack</h4>
+    <div class="stack flex flex-col items-center grow-1">
+      <DecryptedText v-motion-slide-visible-bottom text="My stack" use-original-chars-only animateOn="hover"
+                     class="section-title text-highlight-2 inline"/>
       <FallingItems/>
     </div>
 
@@ -60,7 +68,7 @@ import FallingItems from "@/components/bits/FallingItems.vue";
 </template>
 
 <style>
-.stack{
+.stack {
   min-height: 10rem;
 }
 
