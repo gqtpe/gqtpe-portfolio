8b680e434a8a5f20ea5825fcdc8ea3e512e8bdbc
gqtpe <work.temirtas@gmail.com>
2025-03-14 23:47:01 +0500

    portfolio v0.0.6: loading component created

---
+++ src/components/Loading.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import CountUp from "@/components/CountUp.vue";
+</script>
+
+<template>
+  <div class="wrapper flex items-center justify-center p-6">
+    <CountUp :endValue="100" :duration="0.6" />
+
+  </div>
+</template>
+
+<style scoped>
+.wrapper{
+  width: 100vw;
+  height: 100vh;
+ background: var(--color-black);
+  color: white;
+
+}
+</style>
