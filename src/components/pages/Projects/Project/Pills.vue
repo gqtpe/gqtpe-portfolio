2e4933f505dbd3a87e91948f0f3e065c4e024b10
gqtpe <work.temirtas@gmail.com>
2025-03-18 12:27:24 +0500

    portfolio v0.0.6: Pills component created

---
+++ src/components/pages/Projects/Project/Pills.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type {Pill} from "@/components/pages/Projects/types.ts";
+
+defineProps<{ pills: Pill[] }>()
+
+</script>
+
+<template>
+  <div class="pills">
+    <span v-for="pill in pills" class="pill">{{ pill }}</span>
+  </div>
+</template>
+
+<style scoped>
+</style>
