78d9b967d7361c82ed4f4e72b369e50ca4d37000
gqtpe <work.temirtas@gmail.com>
2025-03-19 08:15:34 +0500

    portfolio v0.0.6(simple stack):simple stack component created

---
+++ src/components/pages/About/SimpleStack.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { aboutSVGs } from "@/assets/svg";
+</script>
+
+<template>
+  <div class="wrapper">
+      <img loading="lazy" v-for="svg in aboutSVGs" :src="svg" alt="stack-item">
+  </div>
+</template>
+
+<style scoped>
+  .wrapper{
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    img{
+      width: 2rem;
+    }
+  }
+</style>
