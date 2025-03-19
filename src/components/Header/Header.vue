0ea33d6aa9ae2d3072c02d7068b558d7fb44a985
gqtpe <work.temirtas@gmail.com>
2025-03-19 08:26:50 +0500

    portfolio v0.0.6(responsive header):z index added for header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -6,7 +6,7 @@ import DecryptedText from "@/components/bits/DecryptedText.vue";
 </script>
 
 <template>
-  <header v-motion-slide-top class="page-wrapper flex justify-between items-center">
+  <header v-motion-slide-top class="z-20 page-wrapper flex justify-between items-center">
       <DecryptedText text="GQTPE" use-original-chars-only class="logo font-black" />
       <Navbar/>
     <a target="_blank" :href="links.telegram">
