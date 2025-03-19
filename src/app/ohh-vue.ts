1447c598420d7de5efb78b0b0ecd326b352fa3aa
gqtpe <work.temirtas@gmail.com>
2025-03-19 08:22:27 +0500

    portfolio v0.0.6(responsive header): burger icon added

--- src/app/ohh-vue.ts
+++ src/app/ohh-vue.ts
@@ -7,10 +7,10 @@ import {
     FaExpand,
     FaExternalLinkSquareAlt,
     BiLink45Deg,
-
+    CoHamburgerMenu
 } from "oh-vue-icons/icons";
 
-addIcons(BiGithub,FaExpand, FaExternalLinkSquareAlt, CoTrello,BiFileEarmarkPerson , BiSpotify, BiLink45Deg);
+addIcons(BiGithub,FaExpand, FaExternalLinkSquareAlt, CoTrello,BiFileEarmarkPerson , BiSpotify, BiLink45Deg, CoHamburgerMenu);
 
 
 export {OhVueIcon}
