5dfa684bc85d8737d0e9b269592cfb1566f9cb3c
gqtpe <work.temirtas@gmail.com>
2025-03-19 08:25:07 +0500

    portfolio v0.0.6(responsive header):refactoring

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -4,16 +4,16 @@ import {useRoute} from "vue-router";
 
 const router = useRoute()
 
+const active = ref<number>(0)
 const links = [
   {id: 1, title: "Home", href: "/"},
   {id: 2, title: "About", href: "/about"},
   {id: 3, title: "Projects", href: "/projects"},
   {id: 4, title: "Contacts", href: "/contacts"},
 ];
-const active = ref<number>(0)
 onMounted(() => {
   const value = links.find(l => l.href === window.location.pathname)
-  if(value){
+  if (value) {
     active.value = value.id
   }
 })
@@ -65,9 +65,32 @@ const onMouseEnter = (id: number) => {
 .burger {
   display: none;
 }
+@media (max-width: 40rem) {
+  nav{
+    padding: 1rem;
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    background: #ffffff;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    gap: 1rem;
+    top: 0;
+    left: 100%;
+    z-index: 11;
+    transition: transform, 0.2s ease-in-out;
+  }
+  nav.show{
+    transform: translateX(-100%);
+  }
+  nav.hide{
+    transform: translateX(0);
+  }
   .burger{
     display: block;
   }
+}
 
 .link-wrapper {
   overflow: hidden;
