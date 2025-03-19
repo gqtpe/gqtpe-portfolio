aa96fdce855bb9dd867e43c6a5ac6c3906ed986d
gqtpe <work.temirtas@gmail.com>
2025-03-19 08:21:22 +0500

    portfolio v0.0.6(simple stack):except routes added

--- src/components/Side.vue
+++ src/components/Side.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
-defineProps({title:String})
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const props = defineProps<{ except?: string[] }>();
+
+const show = computed(() => {
+  const except = new Set(props.except || []);
+
+  // home page '/' case
+  if (except.has("/") && route.path === "/") {
+    return false;
+  }
+
+  // checking except equality with `route.path`
+  if (Array.from(except).some(e => e !== "/" && route.path === e)) {
+    return false;
+  }
+
+  return true;
+});
 </script>
 
 <template>
-  <aside  v-motion-fade-visible class="flex bg-black p-4 w-1/4 h-full">
+  <aside v-if="show" v-motion-fade-visible class="flex bg-black p-4 w-1/4 h-full">
     <div class="border-b-2 border-l-2 border-white w-full flex items-end p-4">
-      <h2 class="text-xl uppercase font-bold text-white">{{ title }}</h2>
+      <h2 class="text-xl uppercase font-bold text-white">{{ route.path.slice(1) }}</h2>
     </div>
   </aside>
 </template>
