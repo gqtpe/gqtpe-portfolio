3c0841aa83f80548156f929f83d633dc7a28c470
gqtpe <work.temirtas@gmail.com>
2025-03-20 10:25:41 +0500

    portfolio v0.0.7:download property added to button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,6 +2,7 @@
 type Props = {
   color: 'primary' | 'secondary'
   link?: string
+  download?: string
 }
  defineProps<Props>()
 //color variant style cases
@@ -10,9 +11,9 @@ const primaryClasses = "bg-primary-500 hover:bg-secondary-500"
 </script>
 
 <template>
-  <button :class="`${color==='primary'?primaryClasses:secondaryClasses} transition-colors text-white font-bold py-2 px-4 rounded-full`">
   <a
       :href="link"
+      v-bind="download ? { download } : {}"
       :target="link?'_blank':'_self'">
   <button
            :class="`${color==='primary'?primaryClasses:secondaryClasses} transition-colors text-white font-bold py-2 px-4 rounded-full`">
