3c77f33ddac8d06c96cd627b18a429e438b121ad
gqtpe <work.temirtas@gmail.com>
2025-03-20 10:23:13 +0500

    portfolio v0.0.7: Info page moved to separate component with own props

---
+++ src/components/pages/About/Info.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import Section from "@/components/pages/About/Section.vue";
+import {getSlidePreset} from "@/motion/motion-presets.ts";
+import DecryptedText from "@/components/bits/DecryptedText.vue";
+import Button from "@/components/Button.vue"
+import type {InfoPage} from "@/components/pages/About/about.ts";
+
+defineProps<InfoPage>()
+</script>
+
+<template>
+  <div class="flex p-4 gap-4 mb-8  md:items-start max-md:flex-col items-center">
+    <div class="flex flex-col items-center about__image basis-1/6 ">
+      <img v-motion-roll-visible-once-top loading="lazy" class="" :src="ava" alt="">
+      <Button
+          link="/CV.pdf"
+          class="mt-4"
+          v-motion="getSlidePreset({delay: 1000, initDistance: 50})"
+          color="secondary"
+      >
+        Download CV
+      </Button>
+    </div>
+    <div class="about__info basis-5/6 max-md:text-center">
+      <h3 class="uppercase mb-6 font-bold text-2xl flex items-center gap-2">
+        <DecryptedText :text="hero" animateOn="view" use-original-chars-only></DecryptedText>
+        <div v-motion="getSlidePreset({delay: 1100, initDistance: 50})" class="flex items-center justify-center">
+          <v-icon v-for="icon in icons" hover animation="flash" :scale="1.2" :fill="icon.fill" :name="icon.name"/>
+        </div>
+      </h3>
+      <p v-motion="getSlidePreset({delay: 200, variant: 'visibleOnce'})" class="text-sm text-gray-600">
+        {{subtitle}}
+      </p>
+      <div class="divider"/>
+      <div class="grid grid-cols-2 gap-8 max-md:flex flex-col">
+        <div v-for="(card, i) in cards" v-motion="getSlidePreset({delay: (i+1)*200})">
+          <DecryptedText :text="card.cardTitle" use-original-chars-only animateOn="hover" class="section-title"/>
+          <Section
+              :title="card.title"
+              :period="card.period"
+              :body="card.body"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.icons{}
+</style>
