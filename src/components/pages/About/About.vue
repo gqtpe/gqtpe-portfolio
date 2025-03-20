f70017324d1049103c5ea122a6c59e6dd0cf25a8
gqtpe <work.temirtas@gmail.com>
2025-03-20 10:24:57 +0500

    portfolio v0.0.7: refactoring

--- src/components/pages/About/About.vue
+++ src/components/pages/About/About.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
-import cv from "@/assets/CV.pdf";
-import ava from "@/assets/img/ava_corrected.png"
-import Button from "@/components/Button.vue";
-import SectionT from "@/components/pages/About/Section.vue";
 import DecryptedText from "@/components/bits/DecryptedText.vue";
 import FallingItems from "@/components/bits/FallingItems.vue";
-import {getSlidePreset} from "@/motion/motion-presets.ts";
 import SimpleStack from "@/components/pages/About/SimpleStack.vue";
 import Info from "@/components/pages/About/Info.vue";
 import aboutPage from "./about.ts";
 const windowWidth = window.innerWidth
+
 </script>
 
 <template>
-  <section  class="flex flex-col">
+  <section class="flex flex-col">
     <h3 class="text-highlight-1 mb-4 max-sm:mb-0 self-start">who i am</h3>
     <Info v-bind="aboutPage.info"/>
     <div class="stack flex flex-col items-center grow-1">
@@ -22,7 +18,6 @@ const windowWidth = window.innerWidth
       <FallingItems v-if="windowWidth >= 768" :svgs="aboutPage.stack"/><!--optimization for mobile devices-->
       <SimpleStack v-else :svgs="aboutPage.stack"/>
     </div>
-
   </section>
 </template>
 
@@ -30,10 +25,9 @@ const windowWidth = window.innerWidth
 .stack {
   min-height: 10rem;
 }
-
-
 .about__image {
   max-width: 15rem;
+
   img {
     border-radius: 1rem;
     aspect-ratio: 1/1;
