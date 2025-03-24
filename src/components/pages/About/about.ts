5ddd226b998912f6f9a25087724dfe636e7c077e
gqtpe <work.temirtas@gmail.com>
2025-03-24 22:12:11 +0500

    portfolio v0.0.7:link property added

--- src/components/pages/About/about.ts
+++ src/components/pages/About/about.ts
@@ -1,5 +1,6 @@
 import ava from "@/assets/img/ava_corrected.png";
 import { aboutSVGs } from "@/assets/svg";
+import {links} from "@/app/links.ts";
 type AboutPage ={
     info: InfoPage
     stack: string[]
@@ -9,6 +10,7 @@ export type InfoPage = {
     icons: {
         name: string
         fill: string
+        link: string
     }[]
     hero: string
     subtitle: string
@@ -23,11 +25,11 @@ const aboutPage:AboutPage = {
     info:{
         ava: ava,
         icons: [
-            {fill: "#E4405F", name: "bi-instagram",},
-            {fill: "#0088CC", name: "bi-telegram",},
-            {fill: "#0A66C2", name: "bi-linkedin",},
-            {fill: "#181717", name: "bi-github",},
-            {fill: "#AD2C27", name: "co-codewars",},
+            {fill: "#E4405F", name: "bi-instagram",link: links.instagram},
+            {fill: "#0088CC", name: "bi-telegram",link: links.telegram},
+            {fill: "#0A66C2", name: "bi-linkedin",link: links.linkedin},
+            {fill: "#181717", name: "bi-github",link: links.github},
+            {fill: "#AD2C27", name: "co-codewars",link: links.codewars},
         ],
         hero: "i'm Nursain Temirtas",
         subtitle: "A Front-end developer with one year of freelance experience (1 year). I have strong skills in modern technologies like React, Vite, TypeScript, and Redux Toolkit. I build scalable and well-structured applications. I like learning new things, discussing ideas with the team, and finding the best solutions together. I also have some Back-end experience, which helps me understand the full development process and work better with Back-end developers. I'm a constantly developing person, open-minded and dedicated.",
