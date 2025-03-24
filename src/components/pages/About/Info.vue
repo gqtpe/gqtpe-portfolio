c9b6680334805900bb1a82a49c9e6fc460cbedab
gqtpe <work.temirtas@gmail.com>
2025-03-24 22:11:48 +0500

    portfolio v0.0.7:link added

--- src/components/pages/About/Info.vue
+++ src/components/pages/About/Info.vue
@@ -25,7 +25,7 @@ defineProps<InfoPage>()
       <h3 class="uppercase mb-6 font-bold text-2xl flex items-center gap-2">
         <DecryptedText :text="hero" animateOn="view" use-original-chars-only></DecryptedText>
         <div v-motion="getSlidePreset({delay: 1100, initDistance: 50})" class="flex items-center justify-center">
-          <v-icon v-for="icon in icons" hover animation="flash" :scale="1.2" :fill="icon.fill" :name="icon.name"/>
+          <a target="_blank" v-for="icon in icons" :href="icon.link"><v-icon hover animation="flash" :scale="1.2" :fill="icon.fill" :name="icon.name"/></a>
         </div>
       </h3>
       <p v-motion="getSlidePreset({delay: 200, variant: 'visibleOnce'})" class="text-sm text-gray-600">
