ca9dd590c3c1779521e965fa0f258b259d40b5b6
gqtpe <work.temirtas@gmail.com>
2025-03-29 15:09:14 +0500

    portfolio v0.0.7:link adaptive

--- src/components/pages/About/Info.vue
+++ src/components/pages/About/Info.vue
@@ -22,10 +22,12 @@ defineProps<InfoPage>()
       </Button>
     </div>
     <div class="about__info basis-5/6 max-md:text-center">
-      <h3 class="uppercase mb-6 font-bold text-2xl flex items-center gap-2">
+      <h3 class="uppercase mb-6 font-bold text-2xl flex flex-row items-center gap-2 max-md: flex-col">
         <DecryptedText :text="hero" animateOn="view" use-original-chars-only></DecryptedText>
         <div v-motion="getSlidePreset({delay: 1100, initDistance: 50})" class="flex items-center justify-center">
-          <a target="_blank" v-for="icon in icons" :href="icon.link"><v-icon hover animation="flash" :scale="1.2" :fill="icon.fill" :name="icon.name"/></a>
+          <a target="_blank" class="flex items-center justify-center" v-for="icon in icons" :href="icon.link">
+            <v-icon hover animation="flash" :scale="1.2" :fill="icon.fill" :name="icon.name"/>
+          </a>
         </div>
       </h3>
       <p v-motion="getSlidePreset({delay: 200, variant: 'visibleOnce'})" class="text-sm text-gray-600">
