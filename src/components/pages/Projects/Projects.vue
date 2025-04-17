016a8dd06dced46a0b13e452c1655a87a907aeaa
gqtpe <work.temirtas@gmail.com>
2025-04-17 10:56:50 +0500

    portfolio v0.0.7:fixed

--- src/components/pages/Projects/Projects.vue
+++ src/components/pages/Projects/Projects.vue
@@ -1,24 +1,22 @@
 <script setup lang="ts">
 import ProjectCard from "./ProjectCard.vue";
 import projects from "./projects";
-import {getPopupPreset} from "@/motion/motion-presets.ts";
 </script>
 
 <template>
   <section class="text-black">
-      <h3 class="text-highlight-1">latest work</h3>
-      <h6 class="text-comment px-8 pt-4">"Hi! I build scalable, user-friendly web applications. Here are some
-        of my latest projects."</h6>
-      <div class="flex flex-col items-start p-8 gap-8">
-        <h3 class="text-highlight-2">my projects</h3>
-        <div class="cards w-full grid grid-cols-3 gap-2 max-md:grid-cols-2 max-sm:flex flex-col">
-          <ProjectCard v-for="project in projects"
-                       :project="project"
-                       :key="project.id"
-
-          />
-        </div>
+    <h3 class="text-highlight-1">latest work</h3>
+    <h6 class="text-comment px-8 pt-4">"Hi! I build scalable, user-friendly web applications. Here are some
+      of my latest projects."</h6>
+    <div class="flex flex-col items-start p-8 gap-8">
+      <h3 class="text-highlight-2">my projects</h3>
+      <div class="cards w-full grid grid-cols-3 gap-2 max-md:grid-cols-2 max-sm:flex flex-col">
+        <ProjectCard v-for="project in projects"
+                     :project="project"
+                     :key="project.id"
+        />
       </div>
+    </div>
   </section>
 </template>
 
