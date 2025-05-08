15c7363e40123e3a4d8f9b8e02b530e5424762cd
gqtpe <work.temirtas@gmail.com>
2025-05-08 16:46:28 +0500

    portfolio v0.5.0(home):header reformed

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -6,16 +6,35 @@ import DecryptedText from "@/components/bits/DecryptedText.vue";
 </script>
 
 <template>
-  <header v-motion-slide-top class="z-20 page-wrapper flex justify-between items-center dark:text-white dark:bg-black">
+  <header v-motion-slide-top class="text-white dark:text-zinc-200">
+    <div class="header__wrapper glass ">
       <DecryptedText text="GQTPE" use-original-chars-only class="logo font-black" />
       <Navbar/>
       <Button color="primary" :link="links.telegram">
         Let's talk
       </Button>
+    </div>
   </header>
 </template>
 
 <style scoped>
+header{
+  user-select: none;
+  position: absolute;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 20;
+}
+.header__wrapper{
+  display:grid;
+  grid-template-columns: 1fr auto 1fr;
+  align-items: center;
+  gap: 2rem;
+  margin: 0.5rem 0;
+  padding: 0.5rem 1rem;
+}
 .logo{
   font-size: 1.5rem;
 }
