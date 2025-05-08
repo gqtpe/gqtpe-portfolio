3219f8b039d293736b43e95bc90d55ddf469c158
gqtpe <work.temirtas@gmail.com>
2025-05-08 16:46:11 +0500

    portfolio v0.5.0(home):home page not needed anymore

--- src/components/pages/Home/Home.vue
+++ src/components/pages/Home/Home.vue
@@ -1,88 +1,13 @@
 <script setup lang="ts">
-import Button from "@/components/Button.vue";
-import Typing from "@/components/Typing.vue";
-import DecryptedText from "@/components/bits/DecryptedText.vue";
-import {getSlidePreset} from "@/motion/motion-presets.ts";
-import {computed} from "vue";
 
-// switching items on home page
-const items = [
-  'web developer',
-  'frontend developer',
-  'software engineer',
-];
 
-// motion presets
-const buttonMotion = computed(() => getSlidePreset({ delay: 500 ,direction: "bottom" ,variant:'visibleOnce'}));
 </script>
 
 <template>
-  <section class="flex items-center max-md:justify-center dark:bg-zinc-900">
-    <div class="bg-black dark:bg-zinc-100 dark:text-black p-8 flex flex-col gap-[10px] items-start max-md:w-full text-white">
-      <h6 class="ellipse1 uppercase text-lg font-bold dark:text-zinc-500">
-        <DecryptedText
-            text="hello, there"
-            animateOn="view"
-            revealDirection="start"
-        />
-      </h6>
+  <section class="">
 
-      <h1 class="ellipse2 dark:text-zinc-800 uppercase font-black lg:text-7xl sm:text-6xl max-md:text-7xl max-sm:text-3xl">
-        <DecryptedText
-            text="i'm Nursain Temirtas"
-            animateOn="view"
-            revealDirection="start"
-        />
-      </h1>
-
-      <h6 class="uppercase font-bold  dark:text-zinc-500">
-        <Typing :items="items" />
-      </h6>
-
-      <Button
-          v-motion="buttonMotion"
-          color="secondary"
-          link="/CV.pdf"
-      >
-        Download CV
-      </Button>
-    </div>
   </section>
 </template>
 
 <style scoped>
-.ellipse1, .ellipse2 {
-  position: relative;
-  z-index: 1;
-}
-
-.ellipse1:before, .ellipse2:after {
-  content: "";
-  position: absolute;
-  width: 2rem;
-  height: 2rem;
-}
-
-.ellipse1:before {
-  background-color: var(--color-secondary-500);
-  @media(prefers-color-scheme: dark){
-    background-color: var(--color-secondary-300);
-  }
-
-  border-radius: 50%;
-  z-index: -1;
-  bottom: -10%;
-  left: -1%;
-}
-
-.ellipse2:after {
-  background-color: var(--color-primary-500);
-  @media(prefers-color-scheme: dark){
-    background-color: var(--color-primary-200);
-  }
-  border-radius: 50%;
-  z-index: -1;
-  bottom: -10%;
-  right: -1%;
-}
 </style>
