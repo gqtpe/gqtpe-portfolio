40e36976911393d2131282305bab0eae5c55d65a
gqtpe <work.temirtas@gmail.com>
2025-05-09 20:10:51 +0500

    portfolio v0.5.0(home): hero animation done

--- src/components/pages/Home/Home.vue
+++ src/components/pages/Home/Home.vue
@@ -3,6 +3,19 @@ import gsap from "gsap";
 import {SplitText} from "gsap/SplitText"
 import {onBeforeMount, onMounted, useTemplateRef} from "vue";
 
+const hero = useTemplateRef('hero')
+  if (hero.value) {
+    const split = SplitText.create(hero.value, {type: 'chars, lines, words'})
+    gsap.from(split.chars, {
+      stagger: {
+        amount: 0.2,
+        from: 'random'
+      },
+      duration: 0.5,
+      yPercent: 'random([-100, 100])',
+      opacity: 0
+    })
+  }
 
 </script>
 
