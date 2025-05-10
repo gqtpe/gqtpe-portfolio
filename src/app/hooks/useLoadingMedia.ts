3f126a0419e32b8ca18f26d9253f56fd39523f5d
gqtpe <work.temirtas@gmail.com>
2025-05-10 19:34:31 +0500

    portfolio v0.5.0: media loading logic moved to custom hook

---
+++ src/app/hooks/useLoadingMedia.ts
@@ -0,0 +1,28 @@
+import {nextTick, onMounted, ref} from "vue";
+
+
+export const useLoadingMedia = () => {
+    const isLoaded = ref(false);
+    onMounted(async () => {
+        await nextTick(); // Ждём рендеринг
+        await waitForFontsAndImages();
+        setTimeout(() => {
+            isLoaded.value = true
+        }, 500);
+    });
+
+    async function waitForFontsAndImages() {
+        //waiting all fonts is loaded
+        await document.fonts.ready;
+        //waiting all images is loaded
+        const images = Array.from(document.images).map(
+            (img) =>
+                img.complete
+                    ? Promise.resolve()
+                    : new Promise((resolve) => (img.onload = img.onerror = resolve))
+        );
+
+        await Promise.all(images);
+    }
+    return {isLoaded}
+}
