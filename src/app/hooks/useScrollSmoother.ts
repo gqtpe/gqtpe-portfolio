d7984bc390edba5c3c1995ab479afad71ca3511a
gqtpe <work.temirtas@gmail.com>
2025-05-10 19:34:41 +0500

    portfolio v0.5.0: scrollSmoother logic added

---
+++ src/app/hooks/useScrollSmoother.ts
@@ -0,0 +1,32 @@
+import {nextTick, onMounted, onUnmounted, ref, watch} from "vue";
+import {useLoadingMedia} from "@/app/hooks/useLoadingMedia.ts";
+import ScrollSmoother from "gsap/ScrollSmoother";
+
+export function useScrollSmoother() {
+    const smoother = ref<any>(null);
+    const {isLoaded} = useLoadingMedia();
+
+    onMounted(() => {
+        watch(isLoaded, async (newIsLoaded) => {
+            //loading is fulfilled and
+            await nextTick();
+            if (newIsLoaded && !smoother.value) {
+                smoother.value = ScrollSmoother.create({
+                    wrapper: "#main-wrapper",
+                    content: "#main",
+                });
+
+            }
+        });
+
+
+    });
+    onUnmounted(() => {
+        if (smoother.value) {
+            smoother.value.kill();
+        }
+    });
+    const scrollTo = (target: string) =>{
+    }
+    return {isLoaded,scrollTo: scrollTo}
+}
