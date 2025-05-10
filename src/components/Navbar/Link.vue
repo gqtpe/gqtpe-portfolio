e3bc098155499cf2a576a2d80d90f831a675169e
gqtpe <work.temirtas@gmail.com>
2025-05-10 19:30:38 +0500

    portfolio v0.5.0: Link component created

---
+++ src/components/Navbar/Link.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+
+type Props = {
+  id: number
+  title: string
+  link: string
+  active: boolean
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="link-wrapper" :key="id">
+    <div
+        :class="`flex flex-col links ${active && 'active'}`"
+    >
+      <span class="link">{{ title }}</span>
+      <span class="link dark">{{ title }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.show {
+  transform: translateX(100%);
+}
+
+.hide {
+  transform: translateX(0);
+}
+
+.active {
+  transform: translateY(-50%);
+}
+
+.links {
+  transition: transform, 0.2s ease-in-out;
+}
+
+.link {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  height: 2rem;
+  padding: 0 1rem;
+  background: var(--color-zinc-800);
+  color: white;
+  border-radius: 0.25rem;
+}
+
+.link-wrapper {
+  overflow: hidden;
+  max-height: 2rem;
+  height: 2rem;
+
+
+}
+
+.dark {
+  filter: invert(100%);
+}
+</style>
