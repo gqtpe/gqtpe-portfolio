15939bf73434eb9a1e6e51b5720adef78a7bf5ba
gqtpe <work.temirtas@gmail.com>
2025-05-10 19:35:27 +0500

    portfolio v0.5.0(home): refactoring

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,71 +1,52 @@
 <script setup lang="ts">
-import {nextTick, onMounted, ref} from "vue";
-import Header from "@/components/Header/Header.vue";
+import {nextTick, onMounted, ref, watch} from "vue";
 import Loading from "@/components/Loading.vue";
 import Home from "@/components/pages/Home/Home.vue";
+import Header from "@/components/Header.vue"
+import "./App.css"
+import Navbar from "@/components/Navbar/Navbar.vue";
+import {useLoadingMedia} from "@/app/hooks/useLoadingMedia.ts";
+import ScrollSmoother from "gsap/ScrollSmoother";
 
-//value for detect all fonts and images is loaded
-const isLoaded = ref(false);
-onMounted(async () => {
-  await nextTick(); // Ждём рендеринг
-  await waitForFontsAndImages();
-  setTimeout(() => {
-    isLoaded.value = true
 
-  }, 500);
-});
+const {isLoaded} = useLoadingMedia();
+const smoother = ref<any>(null);
+onMounted(() => {
+  watch(isLoaded, async (newIsLoaded) => {
+    //loading is fulfilled and
+    await nextTick();
+    if (newIsLoaded && !smoother.value) {
+      smoother.value = ScrollSmoother.create({
+        wrapper: "#main-wrapper",
+        content: "#main",
+      });
 
-async function waitForFontsAndImages() {
-  //waiting all fonts is loaded
-  await document.fonts.ready;
-  //waiting all images is loaded
-  const images = Array.from(document.images).map(
-      (img) =>
-          img.complete
-              ? Promise.resolve()
-              : new Promise((resolve) => (img.onload = img.onerror = resolve))
-  );
+    }
+  });
 
-  await Promise.all(images);
+})
+
+const scrollTo = (target: string) => {
+  debugger;
+  smoother.value?.scrollTo(target, true, "center center")
 }
 </script>
 
 <template>
-  <template v-if="isLoaded" v-motion-roll-visible-top>
-    <Header/>
-    <main>
-      <Home/>
-      <section class="about">
-
-      </section>
-    </main>
+  <template v-if="isLoaded">
+    <Header>
+      <Navbar :scrollTo="scrollTo"/>
+    </Header>
+    <div id="main-wrapper">
+      <main id="main">
+        <Home id="home"/>
+        <section id="about"></section>
+        <section id="projects"></section>
+      </main>
+    </div>
   </template>
   <template v-if="!isLoaded">
     <Loading/>
   </template>
 </template>
 
-<style>
-
-main {
-  overflow-y: auto;
-  scroll-snap-type: y mandatory;
-  @media (max-width: 1024px) {
-    aside {
-      display: none;
-    }
-
-    section {
-      width: 100%;
-    }
-  }
-
-}
-
-section {
-  scroll-snap-align: start;
-  color: black;
-  height: 100%;
-  max-height: 100%;
-  overflow-y: auto;
-}
