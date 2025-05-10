c37e16c4fc3ded41553e277eac9d52c80e490d89
gqtpe <work.temirtas@gmail.com>
2025-05-10 19:30:11 +0500

    portfolio v0.5.0: added scrollSmooth target

--- src/components/Navbar/links.ts
+++ src/components/Navbar/links.ts
@@ -1,7 +1,12 @@
-
-export const navbarLinks = [
-    {id: 1, title: "Home", href: "/"},
-    {id: 2, title: "About", href: "/about"},
-    {id: 3, title: "Projects", href: "/projects"},
-    {id: 4, title: "Contacts", href: "/contacts"},
+type Links = {
+    id: number
+    title: string
+    href: string
+    target: string
+}
+export const navbarLinks: Links[] = [
+    {id: 1, title: "Home", target: "#home", href: "/"},
+    {id: 2, title: "About", target: "#about", href: "/about"},
+    {id: 3, title: "Projects",target: "#projects", href: "/projects"},
+    {id: 4, title: "Contacts",target: "#contacts", href: "/contacts"},
 ];
