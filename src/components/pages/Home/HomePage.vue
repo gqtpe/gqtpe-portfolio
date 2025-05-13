71be1a1799458c66852be9008701ecd2c4c8440d
gqtpe <work.temirtas@gmail.com>
2025-05-13 22:12:23 +0500

    portfolio v0.5.0: light-glass styles added

---
+++ src/components/pages/Home/HomePage.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Home from "./Home.vue";
+import About from "./About/About.vue";
+import {nextTick, onMounted} from "vue";
+
+type Props = {
+  scrollTo?: string
+  scroll: () => void
+}
+const {scroll, scrollTo} = defineProps<Props>()
+onMounted(async () => {
+  await nextTick();
+  if (scrollTo) {
+    scroll(scrollTo)
+  }
+})
+</script>
+
+<template>
+  <Home id="home"/>
+  <About/>
+</template>
+
+<style scoped>
+
+</style>
