c760bf0dc71a7ee07d9315a3cb3ff0e8565549bd
gqtpe <work.temirtas@gmail.com>
2025-05-14 15:38:57 +0500

    portfolio v0.5.0: renamed Info to About

--- src/components/pages/Home/About/About.vue
+++ src/components/pages/Home/About/About.vue
@@ -1,43 +1,116 @@
 <script setup lang="ts">
-import {onMounted} from 'vue';
-import {gsap} from 'gsap';
-import SplitText from "gsap/SplitText"
-import data from "./about.ts"
-import Info from "@/components/pages/Home/About/Info.vue";
+import Section from "@/components/pages/Home/About/Section.vue";
+import type {InfoPage} from "@/components/pages/Home/About/about.ts";
+import Button from "@/components/Button.vue";
+import DecryptedText from "@/components/bits/DecryptedText.vue";
 
-onMounted(() => {
-  const split1 = SplitText.create('#animation1', {type: 'chars, lines, words'})
-  gsap.from(split1.chars,{
-    opacity: 0,
-    y: 200,
-    stagger: 0.04,
-    duration: 0.5,
-    ease: 'expo.out',
-    scrollTrigger: {
-      trigger: '#animation1',
-      toggleActions: 'restart none resume pause',
-    }
-  });
-
-});
+const {info} = defineProps<{ info: InfoPage }>();
 </script>
 
-<style scoped>
-section {
-  color: white;
-}
-</style>
-
 <template>
-  <section class="whoiam flex justify-center items-center" id="about">
-    <div class="text-7xl font-black text-zinc-500" id="animation1">WHO I AM?</div>
-  </section>
-  <Info :info="data.info"/>
-</template>
-<style scoped>
+  <section id="about" class="flex justify-center">
+    <div class="glass p-[2rem] about__info text-white">
+      <div class="about__image">
+        <img loading="lazy" :src="info.ava" alt=""/>
+      </div>
+      <div class="about__text max-md:text-center">
+        <h3 class="uppercase mb-3 font-bold text-4xl max-md:flex-col">
+          <DecryptedText :text="info.hero" animateOn="view" use-original-chars-only/>
+        </h3>
+        <p class="text-xl text-gray-300 dark:text-gray-300">
+          {{ info.subtitle }}
+        </p>
 
+        <div class="divider"/>
+
+        <div class="grid grid-cols-2 gap-8 max-md:flex flex-col">
+          <div v-for="(card, i) in info.cards" :key="i">
+            <DecryptedText
+                :text="card.cardTitle"
+                use-original-chars-only
+                animateOn="view"
+                class="section-title"
+            />
+            <Section
+                :title="card.title"
+                :period="card.period"
+                :body="card.body"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="about__links flex flex-col justify-around items-center">
+        <Button size="large" link="/CV.pdf" class-name="w-[100%]" color="primary">
+          Download CV
+        </Button>
+        <div class="flex glass-light p-1">
+          <a target="_blank"  v-for="icon in info.icons" :href="icon.link">
+            <v-icon hover animation="flash" :scale="2" :fill="icon.fill" :name="icon.name"/>
+          </a>
+        </div>
+
+      </div>
+    </div>
+
+  </section>
+</template>
+
+<style scoped>
 #about {
-  overflow: hidden;
-  height: 50vh !important;
+  padding: 2.5rem 5rem;
+  background-color: var(--color-primary-500);
+  background-image: var(--section1-gray);
 }
+
+@media (prefers-color-scheme: dark) {
+  #about {
+    background-color: var(--color-gray-400);
+    background-image: var(--section1-gray);
+  }
+}
+
+.about__info {
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-template-columns: auto auto;
+  gap: 1rem;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.about__text {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 1rem;
+  overflow: hidden;
+  grid-column-start: 2;
+  grid-row-start: 1;
+  grid-row-end: -1;
+}
+
+.about__image {
+  height: 29rem;
+  aspect-ratio: 1 / 1;
+  overflow: hidden;
+  border-radius: 2rem;
+  align-self: start;
+  flex-shrink: 0;
+  grid-column-start: 1;
+}
+
+.about__image img {
+  height: 100%;
+  max-height: 100%;
+  max-width: 100%;
+}
+
+.about__links {
+  #button-link{
+    width: 50%;
+  }
+  grid-row: 2;
+}
+
 </style>
