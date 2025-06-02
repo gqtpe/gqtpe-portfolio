520850434e37f7a1ffe50798c801098c76b0b225
gqtpe <work.temirtas@gmail.com>
2025-06-03 04:15:59 +0500

    portfolio v0.5.0(Header|Redirect):page header section styles moved inside component

--- src/components/pages/Projects/Projects.vue
+++ src/components/pages/Projects/Projects.vue
@@ -14,4 +14,25 @@ import projects from "./projects";
   </section>
 </template>
 
-<style scoped></style>
+
+<style scoped>
+.projects-hello {
+  background: var(--color-zinc-800);
+}
+
+.projects-hello {
+  color: white;
+}
+</style>
+<!--    <h3 class="text-highlight-1">latest work</h3>-->
+<!--    <h6 class="text-comment px-8 pt-4">"Hi! I build scalable, user-friendly web applications. Here are some-->
+<!--      of my latest projects."</h6>-->
+<!--    <div class="flex flex-col items-start p-8 gap-8">-->
+<!--      <h3 class="text-highlight-2">my projects</h3>-->
+<!--      <div class="cards w-full grid grid-cols-3 gap-2 max-md:grid-cols-2 max-sm:flex flex-col">-->
+<!--        <ProjectCard v-for="project in projects"-->
+<!--                     :project="project"-->
+<!--                     :key="project.id"-->
+<!--        />-->
+<!--      </div>-->
+<!--    </div>-->
