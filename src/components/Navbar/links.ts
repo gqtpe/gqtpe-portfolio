8b72ba8e8a34c03c0d513c8df7f867d4e346bf80
gqtpe <work.temirtas@gmail.com>
2025-06-02 14:38:11 +0500

    portfolio v0.5.0: links refactored

--- src/components/Navbar/links.ts
+++ src/components/Navbar/links.ts
@@ -5,8 +5,8 @@ export type Link = {
     target?: string
 }
 export const navbarLinks: Link[] = [
-    {id: 0, title: "Home",target:"#home", href: "/"},//todo: disable button if /about route
-    {id: 1, title: "About", target: "#about", href:'/about'},//todo: disable on homePage route
-    {id: 2, title: "Projects", href: "/projects"},
-    {id: 3, title: "Contacts", href: "/contacts"},
+    {id: 0, title: "Home",target:"#home", path: "/"},//todo: disable button if /about route
+    {id: 1, title: "About", target: "#about", path:'/about'},//todo: disable on homePage route
+    {id: 2, title: "Projects", path: "/projects"},
+    {id: 3, title: "Contacts", path: "/contacts"},
 ];
