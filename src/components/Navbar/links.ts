3587db2ff54c2086fa478828d170fd129705cc0d
gqtpe <work.temirtas@gmail.com>
2025-06-04 22:28:51 +0500

    portfolio v0.5.0: added target for project link

--- src/components/Navbar/links.ts
+++ src/components/Navbar/links.ts
@@ -7,6 +7,6 @@ export type Link = {
 export const navbarLinks: Link[] = [
     {id: 0, title: "Home",target:"#home", path: "/"},//todo: disable button if /about route
     {id: 1, title: "About", target: "#about", path:'/about'},//todo: disable on homePage route
-    {id: 2, title: "Projects", path: "/projects"},
+    {id: 2, title: "Projects", path: "/projects", target: "#page-header", disableSmooth: true},
     {id: 3, title: "Contacts", path: "/contacts"},
 ];
