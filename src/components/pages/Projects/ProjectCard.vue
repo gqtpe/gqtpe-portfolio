f358aace3d2ec1ae7a70b7f2e3f4a3372053ee97
gqtpe <work.temirtas@gmail.com>
2025-06-09 20:36:12 +0500

    portfolio v0.5.0: simple parallax integrated

--- src/components/pages/Projects/ProjectCard.vue
+++ src/components/pages/Projects/ProjectCard.vue
@@ -1,68 +1,69 @@
 <script setup lang="ts">
-import {cutFrom} from "@/utils/cutFrom";
 import type {Project} from "@/components/pages/Projects/types";
+import {RouterLink} from "vue-router";
 
 
 type Props = {
   project: Project
 }
 defineProps<Props>()
+
+const isHTTPLink = (url: string) => /^https?:\/\//.test(url)
+
+const getLinkProps = (link) => {
+  if (isHTTPLink(link.url)) {
+    return {
+      is: 'a',
+      href: link.url,
+      target: '_blank',
+    }
+  } else {
+    return {
+      is: RouterLink,
+      to: link.url,
+      target: '_self',
+    }
+  }
+}
 </script>
 
 <template>
-  <div class="card" v-motion-slide-visible-once-left>
-    <div class="card__image relative overflow-hidden flex items-center justify-center">
+  <section class="project-card flex justify-center items-center">
+    <div class="flex flex-col items-center">
       <v-icon
           :name="project.icon"
           class="card__icon"
           fill="var(--color-gray-900)"
       />
-      <div v-if="(!!project.links)"
-           class="card__actions absolute left-0 bottom-0 flex items-center justify-center gap-2">
-        <RouterLink v-if="!!project.links.path" :to="project.links.path" class="card-action">
-          <v-icon
-              :scale="1.5"
-              :hover="true"
-              speed="fast"
-              animation="flash"
-              name="fa-expand"/>
-        </RouterLink>
-        <a v-if="!!project.links.url" target="_blank" class="card-action" :href="project.links.url">
-          <v-icon
-              :scale="1.5"
-              :hover="true"
-              speed="fast"
-              animation="flash"
-              name="fa-external-link-square-alt"/>
-        </a>
-        <a v-if="!!project.links.github" target="_blank" class="card-action" :href="project.links.github">
-          <v-icon
-              :scale="1.5"
-              :hover="true"
-              speed="fast"
-              animation="flash"
-              name="bi-github"/>
-        </a>
-      </div>
+<!--      <div v-if="(!!project.links)" class="card__actions left-0 bottom-0 flex items-center justify-center gap-2">-->
+<!--        <component-->
+<!--            v-for="link in project.links"-->
+<!--            :key="link.url"-->
+<!--            v-bind="getLinkProps(link)"-->
+<!--            class="card-action"-->
+<!--        >-->
+<!--          <v-icon-->
+<!--              :scale="1.5"-->
+<!--              hover-->
+<!--              speed="fast"-->
+<!--              animation="flash"-->
+<!--              :name="link.icon"-->
+<!--          />-->
+<!--        </component>-->
+<!--      </div>-->
     </div>
-    <div class="card__description">
-      <RouterLink :to="project.links.path || '#'"><h3 class="text-link-1 text-lg ">{{ project.title }}</h3></RouterLink>
-      <p class="text-xs text-gray-500">{{ cutFrom(project.description) }}</p>
-    </div>
-  </div>
+  </section>
 </template>
 
 <style scoped>
-.card{
-  min-width: 10rem;
+section {
+  border-bottom: 1px solid black;
 }
 
 .card__actions {
   width: 100%;
   background: rgba(255, 255, 255, 0.5);
-  transform: translateX(-20%);
-  height:20%;
-  opacity: 0;
+  height: 20%;
   transition: opacity 0.1s ease-out, transform 0.1s ease-in-out;
 
   .card-action {
@@ -74,28 +75,13 @@ defineProps<Props>()
   }
 }
 
-.card__description {
-  padding: 0.5rem;
-  margin-right: 1rem;
-}
-.card__icon{
+.card__icon {
   font-size: 3rem !important;
-  width: 3rem !important;
-  height: 3rem !important;
-  @media(max-width: 1440px){
-    width: 5rem !important;
-    height: 5rem !important;
-  }
-}
-.card__image {
-  background: linear-gradient(135deg, var(--color-secondary-200) 0%, var(--color-primary-200) 100%);
-  aspect-ratio: 16/9;
-
-  &:hover {
-    .card__actions {
-      opacity: 1;
-      transform: translateX(0);
-    }
+  width: 10rem !important;
+  height: 10rem !important;
+  @media (max-width: 1440px) {
+    width: 20rem !important;
+    height: 20rem !important;
   }
 }
 </style>
