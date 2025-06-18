1aa8aec1109cd5086235435ab4dce09f5cc3cfca
gqtpe <work.temirtas@gmail.com>
2025-06-18 17:47:43 +0500

    portfolio v0.5.0: moved links to tailwind

--- src/components/pages/Home/About/About.vue
+++ src/components/pages/Home/About/About.vue
@@ -1,122 +1,100 @@
 <script setup lang="ts">
-import Section from "@/components/pages/Home/About/Section.vue";
+import SectionC from "@/components/pages/Home/About/Section.vue";
 import type {InfoPage} from "@/components/pages/Home/About/about.ts";
 import DecryptedText from "@/components/bits/DecryptedText.vue";
 import Button from "@/components/common/Button.vue";
 
-defineProps<{info: InfoPage}>()
+defineProps<{ info: InfoPage }>()
 </script>
 
 <template>
-  <section id="about" class="flex justify-center">
-    <div class="glass p-[2rem] about__info text-white">
-      <div class="about__image">
-        <img loading="lazy" :src="info.ava" alt=""/>
+  <section id="about" class="flex justify-center px-10 py-15 max-sm:px-0.5">
+    <div class="glass p-[2rem] flex justify-between gap-[1rem] text-white max-sm:flex-col max-sm:p-5 max-sm:text-center">
+      <div class="about__header">
+        <div class="about__image rounded-3xl">
+          <img loading="lazy" :src="info.ava" alt=""/>
+        </div>
+        <div class="flex flex-col justify-around items-center gap-3">
+          <Button size="large" link="/CV.pdf" class-name="w-[100%]" color="primary">
+            Download CV
+          </Button>
+          <div class="flex glass-light p-1">
+            <a target="_blank" v-for="icon in info.icons" :href="icon.link">
+              <v-icon hover animation="flash" :scale="2" :fill="icon.fill" :name="icon.name"/>
+            </a>
+          </div>
+        </div>
       </div>
-    <div class="about__text max-md:text-center">
-      <h3 class="uppercase mb-3 font-bold text-4xl max-md:flex-col">
-        <DecryptedText :text="info.hero" animateOn="view" use-original-chars-only/>
-      </h3>
-      <p class="text-xl text-gray-300 dark:text-gray-300">
-        {{ info.subtitle }}
-      </p>
+      <div class="flex flex-col flex-grow self-start gap-2">
+        <h3 class="uppercase mb-3 font-bold text-2xl">
+          <DecryptedText :text="info.hero" animateOn="view" use-original-chars-only/>
+        </h3>
+        <p class="text-xl text-gray-300 dark:text-gray-300">
+          {{ info.subtitle }}
+        </p>
 
-      <div class="divider"/>
+        <div class="divider"/>
 
-      <div class="grid grid-cols-2 gap-8 max-md:flex flex-col">
-        <div v-for="(card, i) in info.cards" :key="i">
-          <DecryptedText
-              :text="card.cardTitle"
-              use-original-chars-only
-              animateOn="view"
-              class="section-title"
-          />
-          <Section
-              :title="card.title"
-              :period="card.period"
-              :body="card.body"
-          />
+        <div class="grid grid-cols-2 gap-8 max-md:flex flex-col ">
+          <div v-for="(card, i) in info.cards" :key="i">
+            <DecryptedText
+                :text="card.cardTitle"
+                use-original-chars-only
+                animateOn="view"
+                class="section-title max-sm:ali"
+            />
+            <SectionC
+                :title="card.title"
+                :period="card.period"
+                :body="card.body"
+            />
+          </div>
         </div>
       </div>
     </div>
-
-    <div class="about__links flex flex-col justify-around items-center">
-      <Button size="large" link="/CV.pdf" class-name="w-[100%]" color="primary">
-        Download CV
-      </Button>
-      <div class="flex glass-light p-1">
-        <a target="_blank"  v-for="icon in info.icons" :href="icon.link">
-          <v-icon hover animation="flash" :scale="2" :fill="icon.fill" :name="icon.name"/>
-        </a>
-      </div>
-
-    </div>
-    </div>
-
   </section>
 </template>
 
 <style scoped>
-#about {
-  padding: 2.5rem 5rem;
-  position: relative;
-  z-index: 1;
+.about__header{
+  display: flex;
+  flex-direction: column;
 }
-#about::before{
+#about::before {
   content: "";
   position: absolute;
   inset: 0;
   background-image: url("src/assets/img/background/about.png");
   background-size: cover;
-  background-position: center;
+  filter: brightness(80%);
   z-index: -1;
 }
+
 @media (prefers-color-scheme: dark) {
-  #about::before{
+  #about::before {
     filter: grayscale(80%);
   }
 }
-.about__info {
-  display: grid;
-  grid-template-rows: auto 1fr;
-  grid-template-columns: auto auto;
-  gap: 1rem;
-  flex-direction: column;
-  overflow: hidden;
-}
 
-.about__text {
+.about__info {
   display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
+  justify-content: space-between;
   gap: 1rem;
   overflow: hidden;
-  grid-column-start: 2;
-  grid-row-start: 1;
-  grid-row-end: -1;
 }
 
 .about__image {
-  height: 29rem;
+  margin-bottom: 1rem;
+  min-width: 10rem;
   aspect-ratio: 1 / 1;
   overflow: hidden;
-  border-radius: 2rem;
-  align-self: start;
-  flex-shrink: 0;
-  grid-column-start: 1;
+
 }
 
 .about__image img {
   height: 100%;
   max-height: 100%;
   max-width: 100%;
-}
-
-.about__links {
-  #button-link{
-    width: 50%;
-  }
-  grid-row: 2;
 }
 
 </style>
