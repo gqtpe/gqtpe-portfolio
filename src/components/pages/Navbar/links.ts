91872cd1d69050f2fbe0a266e500836c6e9722c6
gqtpe <work.temirtas@gmail.com>
2025-06-19 18:29:52 +0500

    portfolio v0.5.0:Home NavLink changed to Index

--- src/components/pages/Navbar/links.ts
+++ src/components/pages/Navbar/links.ts
@@ -6,7 +6,7 @@ export type Link = {
     disableSmooth?: boolean
 }
 export const navbarLinks: Link[] = [
-    {id: 0, title: "Home",target:"#home", path: "/"},//todo: disable button if /about route
+    {id: 0, title: "Index",target:"#home", path: "/"},//todo: disable button if /about route
     {id: 1, title: "About", target: "#about", path:'/about'},//todo: disable on homePage route
     {id: 2, title: "Projects", path: "/projects", target: "#page-header", disableSmooth: true},
     {id: 3, title: "Contacts", path: "/contacts"},
