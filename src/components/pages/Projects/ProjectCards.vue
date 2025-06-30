d0479cb26568fd27a94b1acd742620b02fc2a118
gqtpe <work.temirtas@gmail.com>
2025-06-30 15:27:58 +0500

    portfolio v0.5.0:scrollTrigger cleaning added

--- src/components/pages/Projects/ProjectCards.vue
+++ src/components/pages/Projects/ProjectCards.vue
@@ -1,28 +1,43 @@
 <script setup lang="ts">
 import projectCards from "@/components/pages/Projects/projectCards.ts";
-import {onMounted} from "vue";
+import {onMounted, onBeforeUnmount, nextTick} from "vue";
 import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 
+const localTriggers: ScrollTrigger[] = [];
 
+const killLocalTriggers = () => {
+  localTriggers.forEach(trigger => trigger.kill());
+  localTriggers.length = 0;
+};
 
+const initProjectAnimations = async () => {
+  await nextTick();
+  killLocalTriggers(); // Удаляем только свои
 
-onMounted(() => {
-  const sections = gsap.utils.toArray<HTMLElement>(".projects__item");
-  const cards = gsap.utils.toArray<HTMLElement>(".project__card");
+  const sections = gsap.utils.toArray<HTMLElement>(".project__item");
+  const cards = gsap.utils.toArray<HTMLElement>(".project__inner");
+
+  if (sections.length === 0 || cards.length === 0) {
+    console.warn("GSAP: элементы не найдены");
+    return;
+  }
 
   sections.forEach((section) => {
-    gsap.to(section, {
+    const tween = gsap.to(section, {
       scrollTrigger: {
+        trigger: section,
         pin: true,
         start: "top top",
         end: "+=100%",
-        trigger: section,
       }
     });
+    localTriggers.push(tween.scrollTrigger!);
   });
+
   cards.forEach((card, i) => {
-    gsap.to(card, {
+    const tween = gsap.to(card, {
       yPercent: 100,
       scrollTrigger: {
         trigger: sections[i],
@@ -31,34 +46,61 @@ onMounted(() => {
         end: "+=100%",
       },
     });
-
+    localTriggers.push(tween.scrollTrigger!);
   });
-  gsap.to(".projects__inner", {
+
+  const pinInner = gsap.to(".project__inner-wp", {
     scrollTrigger: {
-      trigger: ".projects__inner",
+      trigger: ".project__inner-wp",
       start: "top top",
       end: `+=${(sections.length - 1) * 100}%`,
       pin: true,
     },
   });
+  localTriggers.push(pinInner.scrollTrigger!);
+
+  const subtitleTween = gsap.to(".project__subtitle", {
+    yPercent: 100,
+    scrollTrigger: {
+      trigger: sections[0],
+      scrub: true,
+      start: "top bottom",
+      end: "+=100%",
+    }
+  });
+  localTriggers.push(subtitleTween.scrollTrigger!);
+};
+
+onMounted(() => {
+  initProjectAnimations();
+});
+
+onBeforeUnmount(() => {
+  killLocalTriggers(); // Очищаем только свои ScrollTrigger'ы
 });
 </script>
 
+
+
 <template>
 
-  <section class="projects relative">
-    <div class="projects__inner">
+  <section class="project relative">
+    <div class="project__inner-wp">
+      <span class="project__subtitle">Trello</span>
+      <span class="project__subtitle">01</span>
       <div class="inner">
         <v-icon
             v-for="project in projectCards"
             :key="project.id"
-            class="glass-light project__card"
+            class="glass-light project__inner"
             :name="project.icon"
         />
       </div>
+      <span class="project__subtitle">0{{ projectCards.length }}</span>
+      <span class="project__subtitle">React</span>
     </div>
     <section
-        class="projects__item"
+        class="project__item"
         v-for="project in projectCards"
         :key="project.id"
     >
@@ -67,26 +109,46 @@ onMounted(() => {
   </section>
 </template>
 
-<style scoped>
-.projects__inner {
-  position: absolute;
-  z-index: 100;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
+<style>
+.project {
   display: flex;
   justify-content: center;
   align-items: center;
-}
+  flex-direction: column;
 
-.project__card {
-  width: 100%;
-  height: 100%;
-  aspect-ratio: 1/1;
-  position: absolute;
-  top: -100%;
-  left: 0;
+  .project__inner-wp {
+    position: absolute;
+    padding: 0 1rem;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    gap: 4rem;
+  }
+
+  .project__inner {
+    width: 100%;
+    height: 100%;
+    aspect-ratio: 1/1;
+    position: absolute;
+    top: -100%;
+    left: 0;
+  }
+
+  .project__item {
+    height: 100vh;
+    width: 100vw;
+  }
+
+  .project__subtitle {
+    color: white;
+    font-size: 2rem;
+    font-family: var(--font-main);
+  }
 }
 
 .inner {
@@ -98,20 +160,6 @@ onMounted(() => {
 
   &::-webkit-scrollbar {
     display: none;
-  }
-}
-
-.projects {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-
-.projects__item {
-  height: 100vh;
-  width: 100vw;
-  img{
   }
 }
 
