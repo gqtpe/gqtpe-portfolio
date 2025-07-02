7321483c804301462f3a9a2f9e4dbd03aef42ba4
gqtpe <work.temirtas@gmail.com>
2025-07-02 14:43:03 +0500

    portfolio v0.5.0:disabling cursor on button

--- src/components/common/Button.vue
+++ src/components/common/Button.vue
@@ -22,16 +22,21 @@ const sx = `${color==='primary'?primaryClasses:color === 'secondary'?secondaryCl
      aria-label="button-link"
      :class="`shadow-lg rounded-full ${up?'up':''}`"
      id="button-link"
+     data-cursor-disabled
       v-bind="download ? { download } : {}"
       :target="link?'_blank':'_self'">
     <button
-        :class="`${sx} transition-colors text-white font-bold ${size}`">
+        :class="`${sx}  transition-colors text-white font-bold ${size}`">
       <slot/>
     </button>
   </a>
 </template>
 <style scoped>
+button{
+  cursor: pointer;
+}
 .up {
+
   transition: transform, 0.2s ease-in-out;
   &:hover {
     transform: translateY(-0.2rem);
@@ -51,6 +56,7 @@ const sx = `${color==='primary'?primaryClasses:color === 'secondary'?secondaryCl
   .large{
     font-size: 1.25rem;
     padding: 0.75rem 2rem;
+    cursor: pointer;
   }
   .medium{
     padding: 0.5rem 1rem;
