7c97b6e45b9d2478ec2d2166d60cdec635b22ad8
gqtpe <work.temirtas@gmail.com>
2025-07-04 10:44:50 +0500

    portfolio v0.5.0(loading):removed dependency usages

--- src/app/main.ts
+++ src/app/main.ts
@@ -5,7 +5,6 @@ import '@/styles/tailwind.css';
 import App from './App.vue';
 import {OhVueIcon} from "./ohh-vue";
 import { Splide, SplideSlide } from '@splidejs/vue-splide';
-import {MotionPlugin} from "@vueuse/motion";
 import gsap from "gsap";
 import SplitText from "gsap/SplitText"
 import ScrollTrigger from "gsap/ScrollTrigger"
@@ -19,5 +18,4 @@ createApp(App)
     .component("v-icon", OhVueIcon)
     .component("splide-wrapper",Splide)
     .component("slide", SplideSlide)
-    .use(MotionPlugin)
     .mount('#app')
