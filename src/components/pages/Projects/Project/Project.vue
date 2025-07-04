5c910f79b6f4d71198483dd19aa9e0d5400b8a03
gqtpe <work.temirtas@gmail.com>
2025-07-04 17:51:51 +0500

    portfolio v0.5.0: Project links refactored

--- src/components/pages/Projects/Project/Project.vue
+++ src/components/pages/Projects/Project/Project.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
 import projects from "@/components/pages/Projects/projects.ts";
 import DecryptedText from "@/components/bits/DecryptedText.vue";
-import type {ProjectName} from "@/components/pages/Projects/types.ts";
 import Slide from "@/components/common/Splide.vue";
 import Pills from "@/components/pages/Projects/Project/Pills.vue";
+import {useRoute} from "vue-router";
 
-const props = defineProps<{
-  projectName: ProjectName
-}>()
-const project = projects[props.projectName]
+const {params} = useRoute()
+const project = projects[params.name as "trello" | "portfolio" | "spotify"]
 
 </script>
 
@@ -24,21 +22,21 @@ const project = projects[props.projectName]
       />
     </h2>
     <p class="text-comment text-sm self-start">
-      {{project.description}}
+      {{ project.description }}
     </p>
     <div class="project-page__content">
-        <Slide class="project-page__slider" :images="project.images"/>
-        <aside class="project-page__aside">
-          <a v-if="project.links.url" :href="project.links.url" class="flex items-center">
-            <v-icon
-                :scale="1.5"
-                name="bi-link-45deg"
-            />
-            <span class="text-link-1 ">{{project.links.url.split('//')[1]}}</span>
-          </a>
-          <div v-else class="helper">(preview link not provided)</div>
-          <Pills class="project-page__pills" :pills="project.pills"/>
-        </aside>
+      <Slide class="project-page__slider" :images="project.images"/>
+      <aside class="project-page__aside">
+        <a v-if="project.links.length" v-for="link in project.links" :href="link.url" class="flex items-center">
+          <v-icon
+              :scale="1.5"
+              :name="link.icon"
+          />
+          <span class="text-link-1 ">{{ link.url.split('//')[1] }}</span>
+        </a>
+        <div v-else class="helper">(links not provided)</div>
+        <Pills class="project-page__pills" :pills="project.pills"/>
+      </aside>
     </div>
     <!--       {{JSON.stringify(project)}}-->
     <!--       {{JSON.stringify(route)}}-->
@@ -49,22 +47,26 @@ const project = projects[props.projectName]
 .project-page {
   padding-top: 1rem;
   width: 100% !important;
-  &__aside{
+
+  &__aside {
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
   }
+
   &__content {
     width: 100%;
     margin: 1rem 0;
-    align-self:flex-start;
+    align-self: flex-start;
     display: flex;
     justify-content: flex-start;
     gap: 1rem;
+
     .slider {
       flex-grow: 1;
     }
-    @media(max-width: 40rem){
+
+    @media(max-width: 40rem) {
       flex-direction: column;
       align-items: flex-start;
     }
@@ -81,7 +83,8 @@ const project = projects[props.projectName]
     width: 20rem;
   }
 }
-.helper{
+
+.helper {
   color: var(--color-gray-400)
 }
 
