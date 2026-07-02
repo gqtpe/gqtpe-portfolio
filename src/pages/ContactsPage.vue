c90eb042ecda86372bce27e32b3caf715d2aa73c
gqtpe <work.temirtas@gmail.com>
2026-07-02 10:07:10 +0500

    refactor(contacts): remove button showcase, restore form

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -1,13 +1,6 @@
-<script setup lang="ts">
-import PageHeaderRedirect from "@/components/PageHeaderRedirect/PageHeaderRedirect.vue";
-import Button from "@/shared/ui/Button.vue"
-import Input from "@/shared/ui/Input.vue";
-
-</script>
 <template>
-  <div class="contact-page">
   <PageHeaderRedirect class="header" :subtitles="['Reach Out', 'Say Hello']" text="contact"/>
-  <section class="font-main bg-black text-white flex flex-col justify-center items-center gap-[1rem] px-[0.5rem]">
+  <section class="font-main min-h-screen bg-black text-white flex flex-col justify-center items-center gap-[1rem] px-[0.5rem]">
     <h2 class="font-semibold text-6xl max-sm:3xl">Let's Talk</h2>
     <form class="form flex flex-col justify-center items-center gap-[1rem]">
       <div class="flex justify-between gap-[1rem] w-full max-sm:flex-col">
@@ -15,52 +8,18 @@ import Input from "@/shared/ui/Input.vue";
         <Input name="email" class="input" type="text" placeholder="Your Email"/>
       </div>
       <Input name="about" text-area class="input" type="text" placeholder="About"/>
-<!--      <Captcha @myEvent="handleMyEvent" />-->
-<!--      <Button :disabled="submitDisabled" class="font-normal" variant="white" size="large">Send Mail</Button>-->
-      <section class="bg-gray-500 h-[100vh]">
-        <h1>variants</h1>
-        <div class="flex gap-2">
-      <Button variant="primary" >Primary</Button>
-      <Button variant="secondary" >Secondary</Button>
-      <Button variant="white" >White</Button>
-      <Button variant="black" >Black</Button>
-      <Button variant="gradient" >Gradient</Button>
-        </div>
-        <h1>sizes</h1>
-        <div class="flex gap-2">
-
-        <Button variant="primary" size="small">Primary</Button>
-        <Button variant="primary" size="default">Primary</Button>
-        <Button variant="primary" size="large">Primary</Button>
-        <Button variant="secondary" size="small">Secondary</Button>
-        <Button variant="secondary" size="default">Secondary</Button>
-        <Button variant="secondary" size="large">Secondary</Button>
-        </div>
-        <h1>sizes</h1>
-        <div class="flex gap-2">
-          <Button variant="secondary" size="small">Secondary</Button>
-          <Button variant="secondary" size="default">Secondary</Button>
-          <Button variant="secondary" size="large">Secondary</Button>
-        </div>
-      <h1>up</h1>
-      <div class="flex gap-2">
-        <Button up variant="secondary" size="small">Secondary</Button>
-        <Button up variant="secondary" size="default">Secondary</Button>
-        <Button up variant="secondary" size="large">Secondary</Button>
-      </div>
-        <h1>round</h1>
-        <div class="flex gap-2">
-          <Button variant="secondary" round size="small"><v-icon :scale="3" name="co-hamburger-menu" />Secondary</Button>
-          <Button variant="secondary" round size="default"><v-icon :scale="3" name="co-hamburger-menu" />Secondary</Button>
-          <Button variant="secondary" round size="large">Secondary<v-icon :scale="3" name="co-hamburger-menu" /></Button>
-        </div>
-
-  </section>
+      <Captcha />
+      <Button class="w-full" variant="white" round size="large">Send Mail</Button>
     </form>
   </section>
-  </div>
 </template>
+<script setup lang="ts">
+import PageHeaderRedirect from "@/components/PageHeaderRedirect/PageHeaderRedirect.vue";
+import Button from "@/shared/ui/Button.vue"
+import Input from "@/shared/ui/Input.vue";
+import Captcha from "@/components/Captcha.vue";
 
+</script>
 <style scoped>
 .header {
   background: black;
