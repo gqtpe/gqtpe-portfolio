98e09abf915d7184d2e904d4d4e4bd1d4c252e23
gqtpe <work.temirtas@gmail.com>
2026-07-02 19:55:07 +0500

    feat(contacts): send timestamp for EmailJS {{time}} template variable

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -61,7 +61,7 @@ const onSubmit = async () => {
     await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-        {name: form.name, email: form.email, message: form.message},
+        {name: form.name, email: form.email, message: form.message, time: new Date().toLocaleString()},
         {publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY}
     )
     status.value = 'success'
